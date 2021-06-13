fixed normalizing issue in under-sampling

Forecasting/Tester.py
@@ -291,28 +291,35 @@ def main():
     #              [{'label_name': model_names[1][1] + '-raw', 'line_size': 4},
     #               {'label_name': model_names[1][1] + '-fil', 'line_size': 3}],
     #              ]
-    country = args.dataset
-    modeltype = 'LSTM_Simple_WO_Regions'
-    model_names = [
-        (f'{country}_{modeltype}_Unfiltered_None_14_7', 'LSTM-R-None'),
-        (f'{country}_{modeltype}_Filtered_None_14_7', 'LSTM-F-None'),
-        # ('SL_{modeltype}_Unfiltered_Loss_14_7', 'LSTM-R-Loss'),
-        # ('SL_{modeltype}_Filtered_Loss_14_7', 'LSTM-F-Loss'),
-        (f'{country}_{modeltype}_Unfiltered_Reduce_14_7', 'LSTM-R-Reduce'),
-        (f'{country}_{modeltype}_Filtered_Reduce_14_7', 'LSTM-F-Reduce'),
-    ]
-    plot_data = [[{'label_name': model_names[0][1] + '-raw', 'line_size': 4}, {}],
-                 [{}, {'label_name': model_names[1][1] + '-fil', 'line_size': 3}],
-                 [{'label_name': model_names[2][1] + '-raw', 'line_size': 4}, {}],
-                 [{}, {'label_name': model_names[3][1] + '-fil', 'line_size': 3}],
-                 # [{'label_name': model_names[4][1] + '-raw', 'line_size': 4}, {}],
-                 # [{}, {'label_name': model_names[5][1] + '-fil', 'line_size': 3}],
 
+    # country = args.dataset
+    # modeltype = 'LSTM_Simple_WO_Regions'
+    # model_names = [
+    #     (f'{country}_{modeltype}_Unfiltered_None_14_7', 'LSTM-R-None'),
+    #     (f'{country}_{modeltype}_Filtered_None_14_7', 'LSTM-F-None'),
+    #     # ('SL_{modeltype}_Unfiltered_Loss_14_7', 'LSTM-R-Loss'),
+    #     # ('SL_{modeltype}_Filtered_Loss_14_7', 'LSTM-F-Loss'),
+    #     (f'{country}_{modeltype}_Unfiltered_Reduce_14_7', 'LSTM-R-Reduce'),
+    #     (f'{country}_{modeltype}_Filtered_Reduce_14_7', 'LSTM-F-Reduce'),
+    # ]
+    # plot_data = [[{'label_name': model_names[0][1] + '-raw', 'line_size': 4}, {}],
+    #              [{}, {'label_name': model_names[1][1] + '-fil', 'line_size': 3}],
+    #              [{'label_name': model_names[2][1] + '-raw', 'line_size': 4}, {}],
+    #              [{}, {'label_name': model_names[3][1] + '-fil', 'line_size': 3}],
+    #              # [{'label_name': model_names[4][1] + '-raw', 'line_size': 4}, {}],
+    #              # [{}, {'label_name': model_names[5][1] + '-fil', 'line_size': 3}],
+    #              ]
+    model_names = [
+        ("['SL', 'Texas', 'NG', 'IT']_LSTM_Simple_WO_Regions_Filtered_Reduce_14_7", 'LSTM-ALL-F-Reduce'),
+    ]
+    plot_data = [[{'label_name': model_names[0][1] + '-raw', 'line_size': 4},
+                  {'label_name': model_names[0][1] + '-fil', 'line_size': 3}]
                  ]
+
     show_predictions2(x_data_scalers, resultsDict, predictionsDict, gtDict, model_names, plot_data, use_f_gt=False)
 
-    # show_pred_daybyday(x_data_scalers, resultsDict, predictionsDict, gtDict, model_names, plot_data, use_f_gt=True)
-    # show_pred_evolution(x_data_scalers, resultsDict, predictionsDict, gtDict, model_names, plot_data, use_f_gt=True)
+    show_pred_daybyday(x_data_scalers, resultsDict, predictionsDict, gtDict, model_names, plot_data, use_f_gt=True)
+    show_pred_evolution(x_data_scalers, resultsDict, predictionsDict, gtDict, model_names, plot_data, use_f_gt=True)
 
     # ======================================================================================== ## Comparison of methods
 
@@ -339,7 +346,7 @@ def main():
         err = predictionsDict[method] - gtDict[method]
         abserr = np.abs(err)
         sqderr = err ** 2
-        mape = (abserr / (abs(gtDict[method]) + abs(predictionsDict[method])+1e-5) * 100)
+        mape = (abserr / (abs(gtDict[method]) + abs(predictionsDict[method]) + 1e-5) * 100)
         # mean = 0
         # for r in range(len(resultsDict[method])):
         #     mean += resultsDict[method][r][metric]
@@ -372,7 +379,7 @@ def main():
 
 
 def get_ub_lb(pred, true, n_regions):
-    err = abs((pred - true)**2)
+    err = abs((pred - true) ** 2)
     ub_err = np.sqrt(np.mean(err, axis=-1, keepdims=True)).repeat(n_regions, axis=-1) + pred
     lb_err = -np.sqrt(np.mean(err, axis=-1, keepdims=True)).repeat(n_regions, axis=-1) + pred
     return ub_err, lb_err
@@ -440,7 +447,7 @@ def show_predictions2(x_data_scalers, resultsDict, predictionsDict, gtDict, mode
         x_data, y_data, _ = get_data(filtered=False, normalize=x_data_scalers, data=daily_cases, dataf=daily_filtered,
                                      population=population)
         x_test, y_test, yhat = get_model_predictions(model, x_data, y_data, x_data_scalers)
-        ygt=y_testf if use_f_gt else y_test
+        ygt = y_testf if use_f_gt else y_test
         if len(plot[0].keys()) != 0:
             resultsDict[f'{model_label} (R)'] = evaluate(ygt, yhat)  # raw predictions v raw true values
             predictionsDict[f'{model_label} (R)'] = yhat
@@ -560,7 +567,7 @@ def show_pred_daybyday(x_data_scalers, resultsDict, predictionsDict, gtDict, mod
     Xf = np.expand_dims(x_dataf[split_days - 14:split_days, :], 0)
     # X = np.expand_dims(x_data[:split_days,:],0)
     # Xf = np.expand_dims(x_dataf[:split_days,:],0)
-    Y = y_data[split_days :, :]
+    Y = y_data[split_days:, :]
     Yf = y_dataf[split_days:, :]
 
     Ys = [Y]

Forecasting/dataset_tester.py
@@ -11,16 +11,15 @@ def main():
     # for country in countries:
     #     print('\nregion is: ' + country)
 
-    daily_split_filtered, daily_split = load_multiple_data(DATASETS=countries, data_path=dataset_path,
+    fil, raw, fs = load_multiple_data(DATASETS=countries, data_path=dataset_path,
                                                          look_back_window=look_back_window,
                                                          window_slide=window_slide, R_EIG_ratio=R_EIG_ratio,
                                                          R_power=R_power, midpoint=False)
-
-    x_train, y_train, _, _, _, _ = load_samples(daily_split_filtered, WINDOW_LENGTH, PREDICT_STEPS)
-    f_train = np.random.random((x_train.shape[0], 2, x_train.shape[2]))  # dummy features
-
-    x_train_uf, y_train_uf, x_train_f = undersample3("ALL", x_train, y_train, f_train, count_h, count_l, num_h, num_l,
-                                                     power_l, power_h, power_penalty, True)
+    temp = load_samples(fil, fs, WINDOW_LENGTH, PREDICT_STEPS)
+    x_train_list, y_train_list, x_test_list, y_test_list, x_val_list, y_val_list, fs_train, fs_test, fs_val = temp
+    x_train, y_train, x_train_feat = undersample3(
+        str(countries), x_train_list, y_train_list, fs_train, count_h, count_l, num_h, num_l, power_l, power_h,
+        power_penalty, True)
 
 
 # %% SCRIPT STARTS HERE

Forecasting/utils/data_loader.py
@@ -16,12 +16,15 @@ def min_max(data):
 
 
 def reduce_regions_to_batch(arrs):
+    # input arrs : (n_arrays, n_regions*, samples*, window)
+    # output : (n_arrays, n_regions x samples, window)
     ret = []
     for arr in arrs:
-        if arr.shape[-1] != 1:
-            ret.append(np.concatenate(arr, -1).T)
-        else:
-            ret.append(arr)
+        new_arr = []
+        for i_regions in range(len(arr)):
+            for i_sample in range(len(arr[i_regions])):
+                new_arr.append(arr[i_regions][i_sample])
+        ret.append(np.array(new_arr))
     return ret
 
 
@@ -124,48 +127,70 @@ def load_smooth_data(DATASET, data_path, look_back_window, window_slide, R_EIG_r
 
     return _x, _x_to_smooth
 
+
 def load_multiple_data(DATASETS, data_path, look_back_window, window_slide, R_EIG_ratio, R_power,
-                     midpoint):
+                       midpoint):
     ret_smooth, ret_raw = [], []
+    features = []
     print(f"Loading datasets {DATASETS}")
     if type(DATASETS) == str:
         DATASETS = DATASETS.split()
     for DATASET in DATASETS:
+
         print(f"Now Loading {DATASET} =)")
-        tmp_smoothed, tmp_raw = load_smooth_data(DATASET, data_path,look_back_window, window_slide, R_EIG_ratio, R_power,
-                     midpoint)
+        tmp_smoothed, tmp_raw = load_smooth_data(DATASET, data_path, look_back_window, window_slide, R_EIG_ratio,
+                                                 R_power,
+                                                 midpoint)
+        features.append(np.zeros((tmp_smoothed.shape[-1], 2)))  # dummy features
 
-
-        tmp_smoothed = reduce_regions_to_batch([tmp_smoothed])[0]
-        tmp_smoothed = expand_dims([tmp_smoothed], 3)[0]
+        # tmp_smoothed = reduce_regions_to_batch([tmp_smoothed])[0]
+        # tmp_smoothed = expand_dims([tmp_smoothed], 3)[0]
         ret_smooth.append(tmp_smoothed)
-        tmp_raw = reduce_regions_to_batch([tmp_raw])[0]
-        tmp_raw = expand_dims([tmp_raw], 3)[0]
+
+        # tmp_raw = reduce_regions_to_batch([tmp_raw])[0]
+        # tmp_raw = expand_dims([tmp_raw], 3)[0]
         ret_raw.append(tmp_raw)
-    #  ret_raw : (datasets, samples*, lookback)
-    # ret_smooth = np.array(ret_smooth)
-    # ret_raw = np.array(ret_raw)
-    ret_raw_np = np.zeros((0, look_back_window, 1))
-    ret_smooth_np = np.zeros((0, look_back_window, 1))
+
+    #  if the shape of ret_raw : (datasets, samples*, lookback, regions*)
+    ret_smooth2, ret_raw2, features2 = [], [], []
     for i in range(len(DATASETS)):
-        ret_raw_np = np.concatenate([ret_raw_np, np.array(ret_raw[i])], 0)
-        ret_smooth_np = np.concatenate([ret_smooth_np, np.array(ret_smooth[i])], 0)
-    return ret_smooth_np, ret_raw_np
+        for j in range(ret_smooth[i].shape[-1]):  # n_regions will be different for each dataset
+            ret_smooth2.append(ret_smooth[i][:, :, j])
+            ret_raw2.append(ret_raw[i][:, :, j])
+            features2.append(features[i][j])
 
-def load_samples(_x, WINDOW_LENGTH, PREDICT_STEPS):
-    X = _x[:, -WINDOW_LENGTH - PREDICT_STEPS:-PREDICT_STEPS, :]
-    Y = _x[:, -PREDICT_STEPS:, :]
-    idx = np.arange(X.shape[0])
-    np.random.shuffle(idx)
-    X_train_idx = np.ceil(len(idx) * 0.8).astype(int)
-    X_train = X[idx[:X_train_idx]]
-    Y_train = Y[idx[:X_train_idx]]
-    X_test = X[idx[X_train_idx:]]
-    Y_test = Y[idx[X_train_idx:]]
-    X_val = np.zeros((0, *X_train.shape[1:]))
-    Y_val = np.zeros((0, *Y_train.shape[1:]))
+    # # if the shape of ret_raw : (datasets, samples*, lookback)
+    # ret_raw2 = np.zeros((0, look_back_window, 1))
+    # ret_smooth2 = np.zeros((0, look_back_window, 1))
+    # for i in range(len(DATASETS)):
+    #     ret_raw2 = np.concatenate([ret_raw2, np.array(ret_raw[i])], 0)
+    #     ret_smooth2 = np.concatenate([ret_smooth2, np.array(ret_smooth[i])], 0)
+    return ret_smooth2, ret_raw2, features2
 
-    return X_train, Y_train, X_test, Y_test, X_val, Y_val
+
+def load_samples(_x, fs, WINDOW_LENGTH, PREDICT_STEPS):
+    # input _x : (regions, samples*, seqlength)
+    # input fs : (regions, features*)
+    x_train_list, y_train_list, x_test_list, y_test_list, x_val_list, y_val_list = [], [], [], [], [], []
+    fs_train, fs_test, fs_val =[], [], []
+    for i_region in range(len(_x)):
+        x = _x[i_region][:, -WINDOW_LENGTH - PREDICT_STEPS:-PREDICT_STEPS]
+        y = _x[i_region][:, -PREDICT_STEPS:]
+        idx = np.arange(x.shape[0])
+        np.random.shuffle(idx)
+        x_train_idx = np.ceil(len(idx) * 0.8).astype(int)
+        x_train_list.append(x[idx[:x_train_idx]])
+        y_train_list.append(y[idx[:x_train_idx]])
+        x_test_list.append(x[idx[x_train_idx:]])
+        y_test_list.append(y[idx[x_train_idx:]])
+        x_val_list.append(np.zeros((0, *x_train_list[-1].shape[1:])))
+        y_val_list.append(np.zeros((0, *y_train_list[-1].shape[1:])))
+
+        fs_train.append(np.repeat(fs[i_region:i_region+1], x_train_list[-1].shape[0],0))
+        fs_test.append(np.repeat(fs[i_region:i_region+1], x_test_list[-1].shape[0],0))
+        fs_val.append(np.repeat(fs[i_region:i_region+1], x_val_list[-1].shape[0],0))
+
+    return x_train_list, y_train_list, x_test_list, y_test_list, x_val_list, y_val_list, fs_train, fs_test, fs_val
 
 
 # def save_train_data(DATASET, data_path, TRAINING_DATA_TYPE, WINDOW_LENGTH, PREDICT_STEPS, midpoint, R_EIG_ratio,